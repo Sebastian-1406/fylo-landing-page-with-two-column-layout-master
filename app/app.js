a00d7423a0592e715d945450b1fd50ce30e8497c
Sebastian <SCsebastian1406@gmail.com>
2021-10-16 12:38:31 -0500

    subiendo diseño desktop, corregido problemas de accesibiliad

--- app/app.js
+++ app/app.js
@@ -1,62 +1,20 @@
-let email = document.querySelectorAll(".email");
-let btnEnviar = document.querySelectorAll(".btn");
-let formulario_1 = document.querySelector(".email_1")
-let formulario_2 = document.querySelector(".email_2")
-
-const expresion = {
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-}
-
+let formulario1 = document.getElementById("formulario__registro");
+let formulario2 = document.getElementById("formulario__registro-2");
+let campo1 = document.getElementById("campo__email")
+let campo2 = document.getElementById("campo__email-2")
+const expresion = {correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,};
 
 const validarCampo = (e) => {
-    e.preventDefault();
-
-    let inputName = e.target.name;
-    let inputValue = e.target.value;
-
-    if(inputName == "email_1"){
-        validarFormulario(inputName, inputValue)
+  e.preventDefault();
+  if (e.target.type == "submit") {
+    if (expresion.correo.test(campo1.value) || expresion.correo.test(campo2.value)) {
+      e.target.parentNode.classList.remove("campo-incorrecto");
+      e.target.parentNode.reset();
+    } else {
+      e.target.parentNode.classList.add("campo-incorrecto");
     }
-    if(inputName == "email_2"){
-        validarFormulario(inputName, inputValue)
-    }
+  }
+};
 
-}
-
-
-const validarFormulario = (name, value) => {
-
-    let expresionEmail = expresion.correo.test(value);
-
-    if(expresionEmail){
-        document.querySelector(`.${name}`).classList.remove("error")
-    }else{
-        document.querySelector(`.${name}`).classList.add("error")
-
-    }
-    
-    formulario_1.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        if(expresionEmail){
-            formulario_1.reset();
-        }
-    })
-    formulario_2.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        if(expresionEmail){
-            formulario_2.reset();
-        }
-    })
-    
-
-}
-
-
-email.forEach(input => {
-    input.addEventListener("click", validarCampo)
-    input.addEventListener("keyup", validarCampo)
-})
-
-
+formulario2.addEventListener("click", validarCampo);
+formulario1.addEventListener("click", validarCampo);
